76c8519a556c197a3a99ae6baf9055f2aa1c7a3a
Ben Spotswood <spotswoodb@gmail.com>
2021-09-14 17:11:56 -0500

    Done.

--- index.js
+++ index.js
@@ -3,38 +3,77 @@
 // it should return a string like "this Saturday, I want to...!"
 // fill in the ... with the activity that's passed in as the first paramater
 
-function saturdayFun(activity="roller-skate") {
-    return `This Saturday, I want to ${activity}!`;
+// function saturdayFun(activity="roller-skate") {
+//     return `This Saturday, I want to ${activity}!`;
+// }
+
+// let mondayWork= function(work="go to the office") {
+//     return `This Monday, I will ${work}.`;
+// }
+
+// function wrapAdjective(flair="*") {
+//     return function(adj="special") {
+//         return `You are ${flair }${adj}${flair}!`;
+//     }
+// }
+
+// const Calculator = {
+//     add: function(a, b) {
+//         return a+b;
+//     },
+//     subtract: function(a, b) {
+//         return a-b;
+//     },
+//     multiply: function(a, b) {
+//         return a*b;
+//     },
+//     divide: function(a, b) {
+//         return a/b;
+//     }
+// }
+
+// function actionApplyer(start, fns) {
+//     for(let i = 0; i < fns.length; i++) {
+//         start = fns[i](start);
+//     }
+//     return start;
+// }
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork= function(work="go to the office") {
-    return `This Monday, I will ${work}.`;
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
 }
-
-function wrapAdjective(flair="*") {
-    return function(adj="special") {
-        return `You are ${flair }${adj}${flair}!`;
+const wrapAdjective = function(hey="*") {
+    return function(param="special"){
+        return `You are ${hey}${param}${hey}!`
     }
 }
 
 const Calculator = {
-    add: function(a, b) {
-        return a+b;
+
+    add: function (a, b){
+        return a+b
     },
-    subtract: function(a, b) {
-        return a-b;
+
+    subtract: function (a, b){
+        return a-b
     },
-    multiply: function(a, b) {
-        return a*b;
+    
+    multiply: function (a, b){
+        return a*b
     },
-    divide: function(a, b) {
-        return a/b;
-    }
+
+    divide: function (a, b){
+        return a/b
+    },
 }
 
-function actionApplyer(start, fns) {
-    for(let i = 0; i < fns.length; i++) {
-        start = fns[i](start);
+function actionApplyer(sinteger, functions){
+    for (let i = 0; i < functions.length; i++){
+        sinteger = functions[i](sinteger)
     }
-    return start;
+    return sinteger
 }
